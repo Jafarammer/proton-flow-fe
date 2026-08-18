2b45cf6ec1aa0140d6d92a085479f4598e3b9fc0
Your Name <jafalkaff96@gmail.com>
2026-08-18 14:47:39 +0700

    stable color compoenent part 1

--- src/utils/Theme.ts
+++ src/utils/Theme.ts
@@ -4,32 +4,53 @@ const primary = "#4F46E5";
 const primaryLight = "#EEF2FF";
 
 export const appTheme: ThemeConfig = {
+  cssVar: {
+    prefix: "ant",
+    key: "proton-flow",
+  },
+
   token: {
+    /* Primary */
     colorPrimary: primary,
     colorInfo: primary,
+
+    /* Status */
     colorSuccess: "#16A34A",
     colorWarning: "#F59E0B",
     colorError: "#DC2626",
+
+    /* Text */
     colorText: "#0F172A",
     colorTextSecondary: "#64748B",
+
+    /* Background */
     colorBgBase: "#FFFFFF",
     colorBgContainer: "#FFFFFF",
+    colorBgLayout: "#F8FAFC",
+
+    /* Border */
     colorBorder: "#CBD5E1",
-    // popup/dropdown item
+    colorBorderSecondary: "#E2E8F0",
+
+    /* Hover */
     controlItemBgHover: primaryLight,
+
+    /* Shape */
     borderRadius: 10,
+
+    /* Font */
     fontFamily: "Ubuntu, sans-serif",
   },
-  components: {
-    Menu: {
-      // normal
-      itemColor: "#0F172A",
-      // hover
-      itemHoverBg: primaryLight,
-      itemHoverColor: primary,
-      // active / selected
-      itemSelectedBg: primaryLight,
-      itemSelectedColor: primary,
-    },
-  },
+
+  // components: {
+  //   Menu: {
+  //     itemColor: "#0F172A",
+
+  //     itemHoverBg: primaryLight,
+  //     itemHoverColor: primary,
+
+  //     itemSelectedBg: primaryLight,
+  //     itemSelectedColor: primary,
+  //   },
+  // },
 };
