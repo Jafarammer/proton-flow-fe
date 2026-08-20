23028b98c419cd92bcb287e71681981a33c1d129
Your Name <jafalkaff96@gmail.com>
2026-08-20 11:07:48 +0700

    implement preview appearance

--- src/features/settings/components/appearance/AppearancePreview.tsx
+++ src/features/settings/components/appearance/AppearancePreview.tsx
@@ -1,7 +1,73 @@
-import React from "react";
+// UI LIbrary
+import { Button, Card, Typography } from "antd";
+// types
+import type { IAppearanceSettings } from "../../types/appearance";
+// styles
+import "../styles/appearance.scss";
 
-const AppearancePreview = () => {
-  return <div>AppearancePreview</div>;
+const { Title, Text } = Typography;
+
+interface AppearancePreviewProps {
+  appearance: IAppearanceSettings;
+}
+
+const AppearancePreview = ({ appearance }: AppearancePreviewProps) => {
+  return (
+    <Card className="preview-card">
+      <div className="preview-card-header">
+        <Title level={4} className="header-title">
+          Live Preview
+        </Title>
+
+        <Text type="secondary" className="header-description">
+          Preview your current settings.
+        </Text>
+      </div>
+
+      <div
+        className="preview-content"
+        style={{ borderRadius: appearance.borderRadius }}
+      >
+        <div
+          className="preview-logo"
+          style={{ backgroundColor: appearance.primaryColor }}
+        >
+          P
+        </div>
+
+        <Title level={3}>Proton Flow</Title>
+        <Text type="secondary">
+          {" "}
+          Build intelligent AI workflows with a simple visual interface.
+        </Text>
+
+        <div className="preview-actions">
+          <Button
+            style={{
+              color: "#fff",
+              backgroundColor: appearance.primaryColor,
+            }}
+          >
+            Primary Actions
+          </Button>
+          <Button>Secondary</Button>
+        </div>
+
+        <div
+          className="preview-info"
+          style={{ borderRadius: appearance.borderRadius }}
+        >
+          <Text
+            style={{
+              color: appearance.secondaryColor,
+            }}
+          >
+            This preview reflects your current appearance settings.
+          </Text>
+        </div>
+      </div>
+    </Card>
+  );
 };
 
 export default AppearancePreview;
--- src/features/settings/components/appearance/AppearanceSettings.tsx
+++ src/features/settings/components/appearance/AppearanceSettings.tsx
@@ -35,29 +35,42 @@ const AppearanceSettings = ({
         <div className="appearance-settings-content">
           {/* theme selector */}
           <ThemeSelector value={appearance.theme} onChange={onChangeTheme} />
-          {/* color selector */}
-          <ColorSelector
-            primaryColor={appearance.primaryColor}
-            secondaryColor={appearance.secondaryColor}
-            onChangePrimary={onChangePrimaryColor}
-            onChangeSecondary={onChangeSecondaryColor}
-          />
-          {/* radius selector */}
-          <RadiusSelector
-            value={appearance.borderRadius}
-            onChange={onChangeBorderRadius}
-          />
-
+          <div className="section-content">
+            {/* color selector */}
+            <ColorSelector
+              primaryColor={appearance.primaryColor}
+              secondaryColor={appearance.secondaryColor}
+              onChangePrimary={onChangePrimaryColor}
+              onChangeSecondary={onChangeSecondaryColor}
+            />
+            {/* radius selector */}
+            <RadiusSelector
+              value={appearance.borderRadius}
+              onChange={onChangeBorderRadius}
+            />
+          </div>
+          {/* actions */}
           <div className="appearance-actions">
-            <Button color="primary" variant="filled" onClick={onReset}>
+            <Button
+              color="primary"
+              variant="filled"
+              onClick={onReset}
+              className="btn"
+            >
               Reset
             </Button>
 
-            <Button color="primary" variant="solid" onClick={onSave}>
+            <Button
+              color="primary"
+              variant="solid"
+              onClick={onSave}
+              className="btn"
+            >
               Save Changes
             </Button>
           </div>
         </div>
+        <AppearancePreview appearance={appearance} />
       </div>
     </div>
   );
